de9fcdaf4259bdda583fb3c29585be461ac10eaf
snekapcs <vsneka276@gmail.com>
2024-09-05 16:43:25 +0530

    server

--- server.js
+++ server.js
@@ -1,64 +1,3 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
-// const multilingualRoutes = require('./routes/multilingualRoutes');
-// const visionRoutes = require('./routes/visionRoutes');
-// const newsRoutes = require('./routes/newsRoutes');
-// const festivalRoutes = require('./routes/festivalRoutes');
-// const contactformRoutes = require('./routes/contactformRoutes');
-// const structureRoutes = require('./routes/structureRoutes');
-// const invitationRoutes = require('./routes/invitationRoutes');
-// const publishRoutes = require('./routes/publishRoutes');
-// const userRoutes = require('./routes/userRoutes');
-// const roleRoutes = require('./routes/roleRoutes');
-// const logger = require('./logger');
-// const cors = require('cors');
-// const { STATUS_CODES } = require('./util/constant');
-
-// require('dotenv').config();
-
-// const app = express();
-// app.use(bodyParser.json());
-// app.use(cors());
-
-// app.use('/public', express.static(__dirname + '/public'));
-
-// // Connect to MongoDB
-// const host = process.env.HOST;
-// const dbname = process.env.DBNAME;
-// const user = process.env.DB_USER;
-// const password = process.env.PASSWORD;
-
-// const uri = `mongodb://${user}:${password}@${host}/${dbname}?authSource=admin`;
-// mongoose.connect(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// })
-//   .then(() => logger.info('Connected to MongoDB'))
-//   .catch(err => logger.error('Failed to connect to MongoDB', { error: err.message }));
-
-// // Use routes
-// app.use('/api', multilingualRoutes);
-// app.use('/api/vision', visionRoutes);
-// app.use('/api/news', newsRoutes);
-// app.use('/api/festival', festivalRoutes);
-// app.use('/api/contact', contactformRoutes);
-// app.use('/api/structure', structureRoutes);
-// app.use('/api/invitation', invitationRoutes);
-// app.use('/api/user', userRoutes);
-// app.use('/api/role', roleRoutes);
-// app.use('/api/publish', publishRoutes);
-
-// app.get("/", (req, res) => {
-//   return res.status(STATUS_CODES.SUCCESS).send(`Welcome to World Tamil siragam 🎉`);
-// })
-// // Start the server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   logger.info(`Server is running on port ${PORT} ${host} ${dbname} ${user} ${password}`);
-// });
-
-
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -85,7 +24,13 @@ app.use(cors());
 app.use('/public', express.static(__dirname + '/public'));
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URL, {
+const host = process.env.HOST;
+const dbname = process.env.DBNAME;
+const user = process.env.DB_USER;
+const password = process.env.PASSWORD;
+
+const uri = `mongodb://${user}:${password}@${host}/${dbname}?authSource=admin`;
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
@@ -110,5 +55,60 @@ app.get("/", (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  logger.info(`Server is running on port ${PORT}`);
+  logger.info(`Server is running on port ${PORT} ${host} ${dbname} ${user} ${password}`);
 });
+
+
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+// const multilingualRoutes = require('./routes/multilingualRoutes');
+// const visionRoutes = require('./routes/visionRoutes');
+// const newsRoutes = require('./routes/newsRoutes');
+// const festivalRoutes = require('./routes/festivalRoutes');
+// const contactformRoutes = require('./routes/contactformRoutes');
+// const structureRoutes = require('./routes/structureRoutes');
+// const invitationRoutes = require('./routes/invitationRoutes');
+// const publishRoutes = require('./routes/publishRoutes');
+// const userRoutes = require('./routes/userRoutes');
+// const roleRoutes = require('./routes/roleRoutes');
+// const logger = require('./logger');
+// const cors = require('cors');
+// const { STATUS_CODES } = require('./util/constant');
+
+// require('dotenv').config();
+
+// const app = express();
+// app.use(bodyParser.json());
+// app.use(cors());
+
+// app.use('/public', express.static(__dirname + '/public'));
+
+// // Connect to MongoDB
+// mongoose.connect(process.env.MONGO_URL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
+//   .then(() => logger.info('Connected to MongoDB'))
+//   .catch(err => logger.error('Failed to connect to MongoDB', { error: err.message }));
+
+// // Use routes
+// app.use('/api', multilingualRoutes);
+// app.use('/api/vision', visionRoutes);
+// app.use('/api/news', newsRoutes);
+// app.use('/api/festival', festivalRoutes);
+// app.use('/api/contact', contactformRoutes);
+// app.use('/api/structure', structureRoutes);
+// app.use('/api/invitation', invitationRoutes);
+// app.use('/api/user', userRoutes);
+// app.use('/api/role', roleRoutes);
+// app.use('/api/publish', publishRoutes);
+
+// app.get("/", (req, res) => {
+//   return res.status(STATUS_CODES.SUCCESS).send(`Welcome to World Tamil siragam 🎉`);
+// })
+// // Start the server
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   logger.info(`Server is running on port ${PORT}`);
+// });
